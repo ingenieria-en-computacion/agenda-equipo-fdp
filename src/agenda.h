8c432a304976666c842ffc0e4593df1ec780e163
MiguelRivas3231 <adriangel12010@gmail.com>
2025-11-22 23:35:55 -0600

    Codigo de Agenda.h
    Updated the structure and definitions in agenda.h to improve clarity and functionality.

--- src/agenda.h
+++ src/agenda.h
@@ -1,39 +1,47 @@
 #ifndef __AGENDA_H__
 #define __AGENDA_H__
 
-#define MAX_CONTACTOS 100
+#include <stdio.h>
+#include <string.h>
 
-enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO};
-enum Mes{ENERO, FEBERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
+//Definir el numero maximo de contactos que seran 100 en nuestro caso
+#define MAX_CONTACTS 100
+
+enum TipoTelefono {CASA, MOVIL, OFICINA, OTRO}; 
+enum Mes {ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE};
 
 struct Persona{
-    char nombre[30];
-    // apellido
-    // mes de nacimiento
-    // dia de nacimient
-    // tipo contacto
-    // numero de telefono
-    // tipo de numero
+    char nombre[30];    // Nombre de la persona
+    char apellido[30];  // Apellido de la persona
+    enum Mes mes_nacimiento;    // Mes de nacimiento
+    int dia_nacimiento;         // Dia de nacimiento
+    char telefono[15];      // Numero de telefono sin importar el tipo
+    enum TipoTelefono tip_num;        // Tipo de telefono ())
 };
 
-typedef struct Persona Contacto;
+typedef struct Persona Contacto;  //
 
 typedef struct Agenda{
-    Contacto contactos[MAX_CONTACTOS];
-    int num_contactos; //Lleva la cuenta de cuantos contactos están en la agenda
+    Contacto contactos[MAX_CONTACTS];   // Arreglo para almacenar los contactos
+    int num_contactos;                  // Lleva la cuenta de cuantos contactos están en la agenda 
 } Agenda;
 
+//Funciones para manejar la agenda
+void iniciar_agenda(Agenda* agenda); //funcion para poner el contador de contactos en 0
+void agregar_contacto(Agenda *agenda, Contacto c); //funcion para que el usuario agregue los datos de un nuevo contacto
+void imprimir_contacto(const Agenda *agenda); //funcion para imrimir el contacto en la pantalla
+int buscar_contacto(Agenda *agenda, char *nombre); //funcion para buscar un contacto por nombre
+int buscar_contacto_x_telefono (Agenda *agenda, char telefono[]); //funcion para buscar un contacto por su número de telefono 
+void ordenar_contactos(Agenda *agenda); //funcion para ordenar los contactos alfabeticamente por nombre
+void mostrar_contactos(Contacto c); //funcion para mostrar todos los contactos de la agenda que se tienen
+void leer_contacto  (Contacto *c); //funcion auxiliar para leer un contacto 
+void cargar_contactos(const char *filename, Agenda *agenda);
+void guardar_contactos(const char *filename, Agenda agenda);
 
-void iniciar_agenda(Agenda *agenda);
-void agregar_contacto(Agenda *agenda, Contacto c);
+// Prototipo para imprimir TODA la agenda
 void imprimir_agenda(Agenda agenda);
-int buscar_contacto(Agenda *agenda, char *nombre);
-int buscar_contacto_x_telefono(Agenda *agenda, char telefono[]);
-void ordenar_contactos(Agenda *agenda);
-void ordenar_contactos_inv(Agenda *agenda);
-void mostrar_contacto(Contacto);
-void leer_contacto(Contacto *c);
-void cargar_contactos(char *filename);
-void guardar_contactos(char *filename);
 
-#endif // __AGENDA_H_
+// Prototipo para imprimir UN SOLO contacto
+void mostrar_contacto(Contacto c);
+
+#endif // __AGENDA_H__
